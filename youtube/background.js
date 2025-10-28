db581fef50bd42f8dafd7e3118b6f1df6b899b8d
Sandeep  Yadav <84236760+hisandeepkumar@users.noreply.github.com>
2025-10-28 21:25:56 +0530

    Create background.js

---
+++ youtube/background.js
@@ -0,0 +1,6 @@
+// Background script for YouTube Gesture Control
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('YouTube Gesture Control extension installed');
+});
+
+// Set up context menu or other background features if needed
