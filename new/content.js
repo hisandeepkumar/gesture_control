627e89ff524baad68bbe0f3e8cc8fab9e46b65bc
Sandeep  Yadav <84236760+hisandeepkumar@users.noreply.github.com>
2025-10-28 23:30:52 +0530

    Update content.js

--- new/content.js
+++ new/content.js
@@ -10,7 +10,7 @@
     script.src = chrome.runtime.getURL('injected.js');
     script.onload = function() {
       this.remove();
-      console.log('🎬 YouTube Keyboard Gestures: Script injected successfully');
+      console.log('🎬 YouTube Camera Gestures: Script injected successfully');
     };
     (document.head || document.documentElement).appendChild(script);
   }
